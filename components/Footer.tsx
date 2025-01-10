f17ff9619ebf9f7872cc6118f4933e8292947528
Nan Xu <nxu2@academic.rrc.ca>
2025-01-10 15:00:46 -0600

    fixing session replay issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const handleLogOut = async () =>{
 
     <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
   <h1 className="text-14 truncate font-normal text-gray-700 font-semibold">
-    {user?.name} //If user is null - Returns undefined instead of crashing
+    {user?.name} {/* If user is null - Returns undefined instead of crashing */}
   </h1>
   <p className="text-14 truncate font-normal text-gray-600">
     {user?.email}
