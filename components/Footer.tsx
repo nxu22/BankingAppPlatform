71af0a0baf0a7e267512406eadc8889d7bf63416
Nan Xu <nxu2@academic.rrc.ca>
2025-01-10 09:39:06 -0600

    logout redirect to signin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,34 +2,35 @@ import React from 'react'
 import Image from 'next/image'
 import { logoutAccount } from '@/lib/actions/user.actions'
 import { useRouter } from 'next/router'
+import { Router } from 'lucide-react'
 
 const footer = ({user,type='desktop'}:FooterProps) => {
 
 const handleLogOut = async () =>{
 
-  const router = useRouter();
+
    const loggedOut = await logoutAccount();
-   if(loggedOut) router.push('/sign-in')
+   if(loggedOut) Router.push('/sign-in')
 } 
 
   return (
     <footer className="footer">
     <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
         <p className='text-xl font-bold text-gray-700'>
-         {user.name[0]}
+         {user?.name[0]}
         </p>
     </div>
 
     <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
   <h1 className="text-14 truncate font-normal text-gray-700 font-semibold">
-    {user.name}
+    {user?.name} //If user is null - Returns undefined instead of crashing
   </h1>
   <p className="text-14 truncate font-normal text-gray-600">
-    {user.email}
+    {user?.email}
   </p>
 </div>
 
-<div className="footer_image" OnClick={handleLogOut}>
+<div className="footer_image" onClick={handleLogOut}>
 <Image src="/icons/logout.svg" fill alt="jsm" />
       </div>
     </footer>
