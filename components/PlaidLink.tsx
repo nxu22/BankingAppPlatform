fbd0116c1eb26b45aade587c1b9577c4c0f62908
Nan Xu <nxu2@academic.rrc.ca>
2025-01-13 15:58:02 -0600

    integrating plaid api

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
+import {PlaidLinkOnSuccess, PlaidLinkOptions} from  'react-plaid-link'
+import { StyledString } from 'next/dist/build/swc/types';
+import { useRouter } from 'next/router';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  const router = useRouter();
+  // 1. State Management
+  const [token, setToken] = useState('');
+
+  // 2. Side Effect for Token Fetching
+  useEffect(() => {
+    const getLinkToken = async () => {
+      
+     // const data = await createLinkToken(user); // Runs once when component mounts
+
+     //setToken(data?.linkToken);
+
+    }
+    getLinkToken();
+  }, []);
+
+
+
+  // 3. Success Callback
+  const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: String) => {
+    //await exchangePublicToken({
+    //  publicToken: public_token,
+    // user,
+    //});
+    router.push('/');
+  }, [user])
+
+  const config: PlaidLinkOptions = {
+    token,
+    onSuccess
+  }
+
   return (
     <>
       {variant === 'primary' ? (
