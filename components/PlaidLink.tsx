d7ced1323fe479c37938f4f869b70ac36c7dd275
Nan Xu <nxu2@academic.rrc.ca>
2025-01-14 12:27:27 -0600

    exchange public token for the access token, connected a payment processor dwolla, transited funds between different accounts

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button'
-import {PlaidLinkOnSuccess, PlaidLinkOptions} from  'react-plaid-link'
+import {PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink} from  'react-plaid-link'
 import { StyledString } from 'next/dist/build/swc/types';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import { createLinkToken } from '@/lib/actions/user.actions';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+  
   const router = useRouter();
   // 1. State Management
   const [token, setToken] = useState('');
@@ -13,13 +15,13 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   useEffect(() => {
     const getLinkToken = async () => {
       
-     // const data = await createLinkToken(user); // Runs once when component mounts
+     const data = await createLinkToken(user); // Runs once when component mounts
 
-     //setToken(data?.linkToken);
+     setToken(data?.linkToken);
 
     }
     getLinkToken();
-  }, []);
+  }, [user]);
 
 
 
@@ -32,16 +34,21 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     router.push('/');
   }, [user])
 
+  //use hook
   const config: PlaidLinkOptions = {
     token,
     onSuccess
   }
 
+  const { open, ready } = usePlaidLink(config);
   return (
     <>
       {variant === 'primary' ? (
-        <Button className='plaidlink-primary'>
-          Connect bank
+        <Button
+        onClick={open}
+          disabled={!ready}
+        className='plaidlink-primary'>
+          connect bank
         </Button>
       ):variant ==="ghost"?(
         <Button>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,69 +1,154 @@
-'use server';
+"use server";
 
 import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
-import { parseStringify } from "../utils";
+import { encryptId, parseStringify } from "../utils";
+import { Languages } from "lucide-react";
+import {
+  CountryCode,
+  ProcessorTokenCreateRequest,
+  ProcessorTokenCreateRequestProcessorEnum,
+  Products,
+} from "plaid";
+import { plaidClient } from "../plaid";
+import { revalidatePath } from "next/cache";
 
-export const signIn = async ({ email, password}:signInProps)=> {
-    try{
-      //get appwrite account client
-      const { account } = await createAdminClient();
-      //create user session with email/password
-      const response = await account.createEmailPasswordSession(email,password);
-      //return stringified session data
-      return parseStringify(response); 
-    }catch(error){
-        console.error('Error', error);
-    }
-} 
+export const signIn = async ({ email, password }: signInProps) => {
+  try {
+    //get appwrite account client
+    const { account } = await createAdminClient();
+    //create user session with email/password
+    const response = await account.createEmailPasswordSession(email, password);
+    //return stringified session data
+    return parseStringify(response);
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
 
-
-export const signUp = async (userData:SignUpParams) => {
-    const {email, password, firstName, lastName }= userData;
-    try{
+export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+  try {
     const { account } = await createAdminClient();
 
-       const newUserAccount =  await account.create(
-        ID.unique(), 
-        email, 
-        password, 
-       `${firstName} ${lastName}`
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
     );
-        const session = await account.createEmailPasswordSession(email, password);
-      
-        (await cookies()).set("appwrite-session", session.secret, {
-          path: "/",
-          httpOnly: true,
-          sameSite: "strict",   
-          secure: true,
-        });
+    const session = await account.createEmailPasswordSession(email, password);
 
-        return parseStringify(newUserAccount);
-    }catch(error){
-        console.error('Error', error);
-        throw error;
-    }
-} 
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
 
+    return parseStringify(newUserAccount);
+  } catch (error) {
+    console.error("Error", error);
+    throw error;
+  }
+};
 
 // checks if a user is currently logged in by attempting to get their account details and returns the user data if logged in, or null if not.
 export async function getLoggedInUser() {
-    try {
-      const { account } = await createSessionClient();
-      const user = await account.get();
-      return parseStringify(user);
-    } catch (error) {
-      return null;
-    }
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+    return parseStringify(user);
+  } catch (error) {
+    return null;
   }
+}
 
-  export const logoutAccount = async () => {
-    try {
-      const {account} = await createSessionClient();
-      cookies().delete('appwrite-session');
-      await account.deleteSession('current');
-    } catch (error) {
-      return null;
-    }
+export const logoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+    cookies().delete("appwrite-session");
+    await account.deleteSession("current");
+  } catch (error) {
+    return null;
   }
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const exchangePublicToken = async ({
+  publicToken,
+  user,
+}: exchangePublicTokenProps) => {
+  try {
+    //Exchange public token for access token and item ID
+    const response = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+    const accessToken = response.data.access_token;
+    const itemId = response.data.item_id;
+    //get account information for plaid using the access token
+    const accountsResonse = await plaidClient.accountsGet({
+      access_token: accessToken,
+    });
+    const accountData = accountsResonse.data.accounts[0];
+
+    const request: ProcessorTokenCreateRequest = {
+      access_token: accessToken,
+      account_id: accountData.account_id,
+      processor: "dwolla" as ProcessorTokenCreateRequestProcessorEnum,
+    };
+    const processorTokenResponse =
+      await plaidClient.processorTokenCreate(request);
+    const processorToken = processorTokenResponse.data.processor_token;
+
+    // Create a funding source URL for the account using the Dwolla customer ID,
+    // processor token, and bank name
+    //help user sending and receive fund
+    const fundingSourceUrl = await addFundingSource({
+      dwollaCustomerId: user.dwollaCustomerId,
+      processorToken,
+      bankName: accountData.name,
+    });
+    //if the funding source url  is not created, throw an error
+    if (!fundingSourceUrl) throw Error;
+
+    // Create a bank account using the user ID, item ID, account ID, access token,
+    // funding source URL, and sharable ID
+    await createBankAccount({
+      userId: user.$id,
+      bankId: itemId,
+      accountId: accountData.account_id,
+      accessToken,
+      fundingSourceUrl,
+      sharableId: encryptId(accountData.account_id),
+    });
+
+    // Revalidate the path to reflect the changes
+    revalidatePath("/");
+
+    // Return a success message
+    return parseStringify({
+      publicTokenExchange: "complete",
+    });
+  } catch (error) {
+    console.error("An error occurred while creating exchanging token:", error);
+  }
+};
